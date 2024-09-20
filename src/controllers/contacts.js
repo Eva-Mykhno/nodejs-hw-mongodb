98a550fa434e8d19c8f84961a5c64da4f6d5b073
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2024-09-20 12:19:44 +0300

    beginnig

---
+++ src/controllers/contacts.js
@@ -0,0 +1,27 @@
+import createHttpError from 'http-errors';
+import { getAllContacts, getContactByID } from '../services/contacts.js';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await getContactByID(contactId);
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
