fedde86ccb73c02bbc2d0b3793dd02f961899c4d
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2024-10-11 13:16:42 +0300

    update service contacts

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -13,7 +13,7 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = ContactsCollection.find();
+  const contactsQuery = ContactsCollection.find({ userId });
 
   if (filter.contactType) {
     contactsQuery.where('contactType').equals(filter.contactType);
@@ -40,7 +40,7 @@ export const getAllContacts = async ({
 };
 
 export const getContactByID = async (contactId, userId) => {
-  const contact = await ContactsCollection.findById(contactId);
+  const contact = await ContactsCollection.findOne({ _id: contactId, userId });
   return contact;
 };
 
